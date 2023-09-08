a9181b3cac2661ca8294c7f1278a25bc2d7d7436
Rneyt <ronaste4ka20100@gmail.com>
2023-09-08 13:10:28 +0300

    initial commit

---
+++ script.js
@@ -0,0 +1,9 @@
+const swiper = new Swiper('.slider', {
+  loop: true,
+	slidesPerGroup: 1,
+	slidesPerView: 'auto',
+  navigation: {
+    nextEl: '.slider-next',
+    prevEl: '.slider-prev',
+  },
+});
